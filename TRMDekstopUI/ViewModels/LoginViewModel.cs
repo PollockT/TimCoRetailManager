50fe576daa38c152e5a7876ea0f91caa7e6c19b9
Theodore Pollock <PollockT@TulsaPreTech.com>
2021-08-26 22:10:14 -0500

    LoginView designed

--- TRMDekstopUI/ViewModels/LoginViewModel.cs
+++ TRMDekstopUI/ViewModels/LoginViewModel.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Caliburn.Micro;
 
 namespace TRMDekstopUI.ViewModels
 {
-    public class ShellViewModel : Conductor<object>
+    public class LoginViewModel
     {
-
     }
 }
