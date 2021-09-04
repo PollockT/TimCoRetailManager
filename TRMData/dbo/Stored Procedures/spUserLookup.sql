3e8a7e751985a3b348087c05cc7163700474ba61
Theodore Pollock <PollockT@TulsaPreTech.com>
2021-09-03 20:34:04 -0500

    User API and DB created and pushed, remember bearer on swagger

---
+++ TRMData/dbo/Stored Procedures/spUserLookup.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id NVARCHAR(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[User]
+	WHERE Id = @Id;
+
+END
+
---
+++ TRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
+using TRMDataManager.Library.Internal.Models;
+
+namespace TRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var parameters = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookUp",
+                                                          parameters,
+                                                          "TRMData");
+            return output;
+        }
+    }
+}
